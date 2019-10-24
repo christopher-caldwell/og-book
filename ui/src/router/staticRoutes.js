2b92bce597496b754e247ecee0dd494f5261f4ab
Christopher Caldwell <christophercaldwell@swbc.com>
2019-10-24 00:31:20 -0500

    building out store

--- ui/src/router/staticRoutes.js
+++ ui/src/router/staticRoutes.js
@@ -1,14 +1,82 @@
-export const loggedInRoutes = [
-  { title: "Home", icon: "dashboard", path: "/" },
-  { title: "Profile", icon: "question_answer", path: "/profile" },
-  { title: "Books", icon: "dashboard", path: "/books" },
-  { title: "My Read Books", icon: "dashboard", path: "/mybooks/read" },
-];
+export const visitorRoutes = [
+  {
+    path: '/Home',
+    icon: 'mdi-account',
+    title: 'Home',
+  },
+  {
+    path: '/register',
+    icon: 'mdi-account',
+    title: 'Register',
+  },
+  {
+    path: '/login',
+    icon: 'mdi-account',
+    title: 'Login',
+  }
+]
 
-export const loggedOutRoutes = [
-  { title: "Home", icon: "dashboard", path: "/" },
-  { title: "Books", icon: "dashboard", path: "/books" },
-  { title: "Login", icon: "question_answer", path: "/login" },
-  { title: "Register", icon: "question_answer", path: "/register" }
-];
-
+export const authenticatedRoutes = [
+  {
+    path: '/Home',
+    icon: 'mdi-account',
+    title: 'Home',
+  },
+  {
+    divider: true,
+    header: 'Create',
+    path: ''
+  },
+  {
+    path: '/home/search/policy',
+    icon: 'mdi-check',
+    title: 'Policy',
+  },
+  {
+    divider: true,
+    header: 'Profile',
+    path: ''
+  },
+  {
+    path: '/user/profile',
+    icon: 'mdi-account-plus',
+    title: 'Admin',
+  },
+]
+export const regularAdminRoutes = [
+  {
+    path: '/home/search/institution',
+    icon: 'mdi-bank',
+    title: 'Institution',
+  },
+  {
+    path: '/home/search/quote',
+    icon: 'mdi-file-document',
+    title: 'Quote',
+  },
+  {
+    path: '/home/search/car',
+    icon: 'mdi-car',
+    title: 'Car',
+  },
+  {
+    path: '/home/search/admin',
+    icon: 'mdi-bank',
+    title: 'Admin',
+  },
+  {
+    path: '/home/search/payment',
+    icon: 'mdi-currency-usd',
+    title: 'Payment',
+  },
+  {
+    path: '/home/search/user',
+    icon: 'mdi-bank',
+    title: 'User',
+  },
+  {
+    path: '/home/search/policy',
+    icon: 'mdi-check',
+    title: 'Policy',
+  },
+]
--- ui/src/store/modules/session.js
+++ ui/src/store/modules/session.js
@@ -1,3 +1,5 @@
+import { timeSessionIsValid } from '@/store/constants'
+
 export default {
   state: {
     token: null,
@@ -9,9 +11,27 @@ export default {
     } 
   },
   actions: {
-    
+    beginNewSession({ commit }, token){
+      commit('UPDATE_SESSION', token)
+    },
+    restoreSessionFromLocalStorage({ commit }){
+      const previousSession = JSON.parse(localStorage.getItem('session'))
+      if(previousSession && previousSession > Date.now()){
+        commit('UPDATE_SESSION', previousSession.token)
+      }
+    },
+    endSession({ commit }){
+      commit('END_SESSION')
+    }
   },
   mutations: {
-
+    UPDATE_SESSION(state, token){
+      state.token = token
+      state.expiredDate = Date.now() + timeSessionIsValid
+    },
+    END_SESSION(state){
+      state.token = ''
+      expiredDate = 0
+    }
   }
 }
